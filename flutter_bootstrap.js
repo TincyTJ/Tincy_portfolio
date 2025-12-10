4d53238565cc240a22a873899d6d09b3a89ec899
TincyTJ <tincyjoseph2002@gmail.com>
2025-12-10 16:33:12 +0530

    first commit web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2336186578"
+    serviceWorkerVersion: "1276911642"
   }
 });
